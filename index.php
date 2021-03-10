dcddd7fd10c573b65dae7516c2d73902036ef862
amalsony505 <mca505@rajagiri.edu>
2021-03-10 15:16:58 +0530

    test

--- index.php
+++ index.php
@@ -9,6 +9,30 @@
     <title>Document</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="test3.html">Login or Signup</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="accordion.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="book.html">Book</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student.html">student</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Rajagiri Kalamassery</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
@@ -66,5 +90,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 </body>
 </html>
---
+++ student.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="test3.html">Login or Signup</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="accordion.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="book.html">Book</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student.html">student</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Rajagiri Kalamassery</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <table class="table">
+        <tr>
+            <td>Name</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Rollno:</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Admno:</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>College</td>
+            <td><input class="form-control" type="text"></td>
+        </tr><tr>
+            <td></td>
+            <td><button class="btn btn-success">Submit</button></td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+</html>
